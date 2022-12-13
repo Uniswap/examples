afd60ba99316e99c4e79f7d2ec6f49645285d823
Adrian Kant <adrian.kant@uniswap.org>
2022-12-13 12:05:30 -0500

    remove wallet example

--- templates/wallet/src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-import { ExternalProvider } from '@ethersproject/providers'
-
-declare global {
-  interface Window {
-    ethereum: ExternalProvider
-  }
-}
--- templates/wallet/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { Currency } from '@uniswap/sdk-core'
-
-export interface Environment {
-  currencyIn: Currency
-  currencyOut: Currency
-  currencyInAmount: string | number
-  currencyOutAmount?: string | number
-  mainnetRpcUrl: string
-}
