61d411a134ca5da857c245925708a01f0e612264
Adrian Kant <adrian.kant@uniswap.org>
2022-12-20 12:05:15 -0500

    first pass of copy and org

---
+++ v3-sdk/routing/src/libs/constants.ts
@@ -0,0 +1,40 @@
+// This file stores web3 related constants such as addresses, token definitions, ETH currency references and ABI's
+
+import { Ether, SupportedChainId, Token } from '@uniswap/sdk-core'
+
+// Addresses
+
+export const V3_SWAP_ROUTER_ADDRESS =
+  '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+
+// Currencies and Tokens
+
+export const ETH = Ether.onChain(SupportedChainId.MAINNET)
+
+export const USDC_TOKEN = new Token(
+  SupportedChainId.MAINNET,
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  6,
+  'USDC',
+  'USD//C'
+)
+
+// ABI's
+
+export const ERC20_WALLET_ABI = [
+  // Read-Only Functions
+  'function balanceOf(address owner) view returns (uint256)',
+  'function decimals() view returns (uint8)',
+  'function symbol() view returns (string)',
+
+  // Authenticated Functions
+  'function transfer(address to, uint amount) returns (bool)',
+
+  // Events
+  'event Transfer(address indexed from, address indexed to, uint amount)',
+]
+
+// Transactions
+
+export const MAX_FEE_PER_GAS = 100000000000
+export const MAX_PRIORITY_FEE_PER_GAS = 100000000000
