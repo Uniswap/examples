d1e6629355a3faeb47ede0b56ed95650eee40ade
dannydaniil <danny.daniil@uniswap.org>
2022-12-22 10:31:43 -0500

    content in folder

---
+++ v3-sdk/collecting-fees/src/config.ts
@@ -0,0 +1,56 @@
+import { Token } from '@uniswap/sdk-core'
+import { FeeAmount } from '@uniswap/v3-sdk'
+import { DAI_TOKEN, USDC_TOKEN } from './libs/constants'
+
+// Sets if the example should run locally or on chain
+export enum Environment {
+  LOCAL,
+  WALLET_EXTENSION,
+  PRODUCTION,
+}
+
+// Inputs that configure this example to run
+export interface ExampleConfig {
+  env: Environment
+  rpc: {
+    local: string
+    mainnet: string
+  }
+  wallet: {
+    address: string
+    privateKey: string
+  }
+  tokens: {
+    token0: Token
+    token0Amount: number
+    token1: Token
+    token1Amount: number
+    poolFee: FeeAmount
+    percentageToRemove: number
+    percentageToAdd: number
+  }
+}
+
+// Example Configuration
+
+export const CurrentConfig: ExampleConfig = {
+  env: Environment.LOCAL,
+  rpc: {
+    local: 'http://localhost:8545',
+    mainnet: 'https://mainnet.infura.io/v3/0ac57a06f2994538829c14745750d721',
+  },
+  wallet: {
+    address: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+    privateKey:
+      '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+  },
+  tokens: {
+    token0: USDC_TOKEN,
+    token0Amount: 1000,
+    token1: DAI_TOKEN,
+    token1Amount: 1000,
+    poolFee: FeeAmount.LOW,
+    percentageToRemove: 1,
+    percentageToAdd: 0.5,
+  },
+}
---
+++ v3-sdk/collecting-fees/src/example/Example.tsx
@@ -0,0 +1,386 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import './Example.css'
+import { ethers } from 'ethers'
+import {
+  Pool,
+  computePoolAddress,
+  Position,
+  nearestUsableTick,
+  NonfungiblePositionManager,
+} from '@uniswap/v3-sdk'
+import { Percent, CurrencyAmount } from '@uniswap/sdk-core'
+import { Environment, CurrentConfig } from '../config'
+import { getCurrencyBalance } from '../libs/balance'
+import { getPositionIds, getTokenTransferApprovals } from '../libs/positions'
+import {
+  connectBrowserExtensionWallet,
+  getProvider,
+  TransactionState,
+  sendTransaction,
+  getWalletAddress,
+} from '../libs/providers'
+import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
+import {
+  POOL_FACTORY_CONTRACT_ADDRESS,
+  MAX_FEE_PER_GAS,
+  MAX_PRIORITY_FEE_PER_GAS,
+  NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
+  DAI_TOKEN,
+  USDC_TOKEN,
+} from '../libs/constants'
+import { fromReadableAmount } from '../libs/conversion'
+
+interface PoolInfo {
+  token0: string
+  token1: string
+  fee: number
+  tickSpacing: number
+  sqrtPriceX96: ethers.BigNumber
+  liquidity: ethers.BigNumber
+  tick: number
+}
+
+const getPoolInfo = async (): Promise<PoolInfo> => {
+  const provider = getProvider()
+  if (!provider) {
+    throw new Error('No provider')
+  }
+
+  const currentPoolAddress = computePoolAddress({
+    factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
+    tokenA: CurrentConfig.tokens.token0,
+    tokenB: CurrentConfig.tokens.token1,
+    fee: CurrentConfig.tokens.poolFee,
+  })
+
+  const poolContract = new ethers.Contract(
+    currentPoolAddress,
+    IUniswapV3PoolABI.abi,
+    provider
+  )
+
+  const [token0, token1, fee, tickSpacing, liquidity, slot0] =
+    await Promise.all([
+      poolContract.token0(),
+      poolContract.token1(),
+      poolContract.fee(),
+      poolContract.tickSpacing(),
+      poolContract.liquidity(),
+      poolContract.slot0(),
+    ])
+
+  return {
+    token0,
+    token1,
+    fee,
+    tickSpacing,
+    liquidity,
+    sqrtPriceX96: slot0[0],
+    tick: slot0[1],
+  }
+}
+
+const getPosition = async (
+  percentageToIncrease?: number
+): Promise<Position> => {
+  // get pool info
+  const poolInfo = await getPoolInfo()
+
+  // construct pool instance
+  const USDC_DAI_POOL = new Pool(
+    CurrentConfig.tokens.token0,
+    CurrentConfig.tokens.token1,
+    poolInfo.fee,
+    poolInfo.sqrtPriceX96.toString(),
+    poolInfo.liquidity.toString(),
+    poolInfo.tick
+  )
+
+  let amount0 = fromReadableAmount(
+    CurrentConfig.tokens.token0Amount,
+    CurrentConfig.tokens.token0.decimals
+  )
+  let amount1 = fromReadableAmount(
+    CurrentConfig.tokens.token1Amount,
+    CurrentConfig.tokens.token1.decimals
+  )
+
+  if (percentageToIncrease) {
+    amount0 = (amount0 * percentageToIncrease) / 100
+    amount1 = (amount1 * percentageToIncrease) / 100
+  }
+
+  // create position using the maximum liquidity from input amounts
+  return Position.fromAmounts({
+    pool: USDC_DAI_POOL,
+    tickLower:
+      nearestUsableTick(poolInfo.tick, poolInfo.tickSpacing) -
+      poolInfo.tickSpacing * 2,
+    tickUpper:
+      nearestUsableTick(poolInfo.tick, poolInfo.tickSpacing) +
+      poolInfo.tickSpacing * 2,
+    amount0,
+    amount1,
+    useFullPrecision: true,
+  })
+}
+
+async function mintPosition(): Promise<TransactionState> {
+  const address = getWalletAddress()
+  const provider = getProvider()
+  if (!address || !provider) {
+    return TransactionState.Failed
+  }
+  // Give approval to the contract to transfer tokens
+  const tokenInApproval = await getTokenTransferApprovals(
+    provider,
+    CurrentConfig.tokens.token0.address,
+    address,
+    NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS
+  )
+  const tokenOutApproval = await getTokenTransferApprovals(
+    provider,
+    CurrentConfig.tokens.token1.address,
+    address,
+    NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS
+  )
+
+  if (
+    tokenInApproval !== TransactionState.Sent ||
+    tokenOutApproval !== TransactionState.Sent
+  ) {
+    return TransactionState.Failed
+  }
+
+  // get calldata for minting a position
+  const { calldata, value } = NonfungiblePositionManager.addCallParameters(
+    await getPosition(),
+    {
+      recipient: address,
+      deadline: Math.floor(Date.now() / 1000) + 60 * 20,
+      slippageTolerance: new Percent(50, 10_000),
+    }
+  )
+
+  // build transaction
+  const transaction = {
+    data: calldata,
+    to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
+    value: value,
+    from: address,
+    maxFeePerGas: MAX_FEE_PER_GAS,
+    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
+  }
+
+  await sendTransaction(transaction)
+  return TransactionState.Sent
+}
+
+async function addLiquidity(positionId: number): Promise<TransactionState> {
+  const address = getWalletAddress()
+  const provider = getProvider()
+  if (!address || !provider) {
+    return TransactionState.Failed
+  }
+
+  const positionToIncreaseBy = await getPosition(
+    CurrentConfig.tokens.percentageToAdd
+  )
+
+  // get calldata for increasing a position
+  const { calldata, value } = NonfungiblePositionManager.addCallParameters(
+    positionToIncreaseBy,
+    {
+      deadline: Math.floor(Date.now() / 1000) + 60 * 20,
+      slippageTolerance: new Percent(50, 10_000),
+      tokenId: positionId,
+    }
+  )
+
+  // build transaction
+  const transaction = {
+    data: calldata,
+    to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
+    value: value,
+    from: address,
+    maxFeePerGas: MAX_FEE_PER_GAS,
+    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
+  }
+
+  await sendTransaction(transaction)
+  return TransactionState.Sent
+}
+
+async function removeLiquidity(positionId: number): Promise<TransactionState> {
+  const address = getWalletAddress()
+  const provider = getProvider()
+  if (!address || !provider) {
+    return TransactionState.Failed
+  }
+
+  const currentPosition = await getPosition()
+
+  // get calldata for minting a position
+  const { calldata, value } = NonfungiblePositionManager.removeCallParameters(
+    currentPosition,
+    {
+      deadline: Math.floor(Date.now() / 1000) + 60 * 20,
+      slippageTolerance: new Percent(50, 10_000),
+      tokenId: positionId,
+      // percentage of liquidity to remove
+      liquidityPercentage: new Percent(CurrentConfig.tokens.percentageToRemove),
+      collectOptions: {
+        expectedCurrencyOwed0: CurrencyAmount.fromRawAmount(DAI_TOKEN, 0),
+        expectedCurrencyOwed1: CurrencyAmount.fromRawAmount(USDC_TOKEN, 0),
+        recipient: address,
+      },
+    }
+  )
+
+  // build transaction
+  const transaction = {
+    data: calldata,
+    to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
+    value: value,
+    from: address,
+    maxFeePerGas: MAX_FEE_PER_GAS,
+    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
+  }
+
+  await sendTransaction(transaction)
+  return TransactionState.Sent
+}
+
+const useOnBlockUpdated = (callback: (blockNumber: number) => void) => {
+  useEffect(() => {
+    const subscription = getProvider()?.on('block', callback)
+    return () => {
+      subscription?.removeAllListeners()
+    }
+  })
+}
+
+const Example = () => {
+  const [tokenInBalance, setTokenInBalance] = useState<string>()
+  const [tokenOutBalance, setTokenOutBalance] = useState<string>()
+  const [positionIds, setPositionIds] = useState<number[]>([])
+  const [txState, setTxState] = useState<TransactionState>(TransactionState.New)
+  const [blockNumber, setBlockNumber] = useState<number>(0)
+
+  // Listen for new blocks and update the wallet
+  useOnBlockUpdated(async (blockNumber: number) => {
+    refreshBalances()
+    setBlockNumber(blockNumber)
+  })
+
+  // Update wallet state given a block number
+  const refreshBalances = useCallback(async () => {
+    const provider = getProvider()
+    const address = getWalletAddress()
+    if (!provider || !address) {
+      throw new Error('No provider or address')
+    }
+    setTokenInBalance(
+      await getCurrencyBalance(provider, address, CurrentConfig.tokens.token0)
+    )
+    setTokenOutBalance(
+      await getCurrencyBalance(provider, address, CurrentConfig.tokens.token1)
+    )
+    setPositionIds(
+      await getPositionIds(
+        provider,
+        address,
+        NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS
+      )
+    )
+  }, [])
+
+  // Event Handlers
+
+  const onConnectWallet = useCallback(async () => {
+    if (await connectBrowserExtensionWallet()) {
+      refreshBalances()
+    }
+  }, [refreshBalances])
+
+  const onMintPosition = useCallback(async () => {
+    setTxState(TransactionState.Sending)
+    setTxState(await mintPosition())
+  }, [])
+
+  const onAddLiquidity = useCallback(async (position: number) => {
+    setTxState(TransactionState.Sending)
+    setTxState(await addLiquidity(position))
+  }, [])
+
+  const onRemoveLiquidity = useCallback(async (position: number) => {
+    setTxState(TransactionState.Sending)
+    setTxState(await removeLiquidity(position))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {CurrentConfig.rpc.mainnet === '' && (
+          <h2 className="error">
+            Please set your mainnet RPC URL in config.ts
+          </h2>
+        )}
+        {CurrentConfig.env === Environment.WALLET_EXTENSION &&
+          getProvider() === null && (
+            <h2 className="error">
+              Please install a wallet to use this example configuration
+            </h2>
+          )}
+        <h3>{`Wallet Address: ${getWalletAddress()}`}</h3>
+        {CurrentConfig.env === Environment.WALLET_EXTENSION &&
+          !getWalletAddress() && (
+            <button onClick={onConnectWallet}>Connect Wallet</button>
+          )}
+        <h3>{`Block Number: ${blockNumber + 1}`}</h3>
+        <h3>{`Transaction State: ${txState}`}</h3>
+        <h3>{`${CurrentConfig.tokens.token0.symbol} Balance: ${tokenInBalance}`}</h3>
+        <h3>{`${CurrentConfig.tokens.token1.symbol} Balance: ${tokenOutBalance}`}</h3>
+        <h3>{`Position Ids: ${positionIds}`}</h3>
+        <button
+          className="button"
+          onClick={onMintPosition}
+          disabled={
+            txState === TransactionState.Sending ||
+            getProvider() === null ||
+            CurrentConfig.rpc.mainnet === ''
+          }>
+          <p>Mint Position</p>
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            onAddLiquidity(positionIds[positionIds.length - 1])
+          }}
+          disabled={
+            txState === TransactionState.Sending ||
+            getProvider() === null ||
+            CurrentConfig.rpc.mainnet === '' ||
+            positionIds.length === 0
+          }>
+          <p>Add Liquidity to Position</p>
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            onRemoveLiquidity(positionIds[positionIds.length - 1])
+          }}
+          disabled={
+            txState === TransactionState.Sending ||
+            getProvider() === null ||
+            CurrentConfig.rpc.mainnet === '' ||
+            positionIds.length === 0
+          }>
+          <p>Remove Liquidity from Position</p>
+        </button>
+      </header>
+    </div>
+  )
+}
+
+export default Example
