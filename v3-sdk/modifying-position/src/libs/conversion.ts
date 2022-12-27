ff149477b3251d31a8916269b97d788029135533
dannydaniil <danny.daniil@uniswap.org>
2022-12-27 10:49:48 -0500

    remove bad merge

--- v3-sdk/modifying-position/src/libs/conversion.ts
+++ v3-sdk/modifying-position/src/libs/conversion.ts
@@ -1,10 +1,6 @@
 export function fromReadableAmount(amount: number, decimals: number): number {
   return amount * Math.pow(10, decimals)
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 export function toReadableAmount(rawAmount: number, decimals: number): number {
   return rawAmount / Math.pow(10, decimals)
 }
