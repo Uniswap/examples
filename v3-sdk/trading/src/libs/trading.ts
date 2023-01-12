a1143052b3032b2e5cab6ba9a6b1823ba68672a9
Dongwu / Hugo <74899114+dongwu-kim@users.noreply.github.com>
2023-01-12 21:47:00 +0900

    fix: change `getTokenTransferApprovals` to `getTokenTransferApproval` (#26)

--- v3-sdk/trading/src/libs/trading.ts
+++ v3-sdk/trading/src/libs/trading.ts
@@ -87,11 +87,7 @@ export async function executeTrade(
   }
 
   // Give approval to the router to spend the token
-  const tokenApproval = await getTokenTransferApprovals(
-    provider,
-    CurrentConfig.tokens.in.address,
-    walletAddress
-  )
+  const tokenApproval = await getTokenTransferApproval(CurrentConfig.tokens.in);
 
   // Fail if transfer approvals do not go through
   if (tokenApproval !== TransactionState.Sent) {
