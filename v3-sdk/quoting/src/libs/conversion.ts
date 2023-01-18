7b2a0fe287fbc17bfb0bb55751bd4a25a718ce65
Danny Daniil <danny.daniil@uniswap.org>
2023-01-18 10:45:54 -0500

    fix: quoting from/to readable (#32)

--- v3-sdk/quoting/src/libs/conversion.ts
+++ v3-sdk/quoting/src/libs/conversion.ts
@@ -1,27 +1,16 @@
-import JSBI from 'jsbi'
+import { BigNumber, ethers } from 'ethers'
 
-export function fromReadableAmount(amount: number, decimals: number): JSBI {
-  const extraDigits = Math.pow(10, countDecimals(amount))
-  const adjustedAmount = amount * extraDigits
-  return JSBI.divide(
-    JSBI.multiply(
-      JSBI.BigInt(adjustedAmount),
-      JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(decimals))
-    ),
-    JSBI.BigInt(extraDigits)
-  )
+const READABLE_FORM_LEN = 4
+
+export function fromReadableAmount(
+  amount: number,
+  decimals: number
+): BigNumber {
+  return ethers.utils.parseUnits(amount.toString(), decimals)
 }
 
 export function toReadableAmount(rawAmount: number, decimals: number): string {
-  return JSBI.divide(
-    JSBI.BigInt(rawAmount),
-    JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(decimals))
-  ).toString()
-}
-
-function countDecimals(x: number) {
-  if (Math.floor(x) === x) {
-    return 0
-  }
-  return x.toString().split('.')[1].length || 0
+  return ethers.utils
+    .formatUnits(rawAmount, decimals)
+    .slice(0, READABLE_FORM_LEN)
 }
