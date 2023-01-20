95f0d90ba070f79be45ee1d09a8f2cb76d3bb452
dannydaniil <danny.daniil@uniswap.org>
2023-01-20 10:49:02 -0500

    implicit return wc

--- web3-react/src/libs/wallet-connect.ts
+++ web3-react/src/libs/wallet-connect.ts
@@ -5,16 +5,17 @@ import { Connection, ConnectionType, onConnectionError } from './connections'
 import { CHAIN_TO_URL_MAP } from './constants'
 
 export function buildWalletConnectConnector() {
-  const [web3WalletConnect, web3WalletConnectHooks] = initializeConnector<WalletConnect>((actions) => {
-    return new WalletConnect({
-      actions,
-      options: {
-        rpc: CHAIN_TO_URL_MAP,
-        qrcode: true,
-      },
-      onError: onConnectionError,
-    })
-  })
+  const [web3WalletConnect, web3WalletConnectHooks] = initializeConnector<WalletConnect>(
+    (actions) =>
+      new WalletConnect({
+        actions,
+        options: {
+          rpc: CHAIN_TO_URL_MAP,
+          qrcode: true,
+        },
+        onError: onConnectionError,
+      })
+  )
   const walletConnectConnection: Connection = {
     connector: web3WalletConnect,
     hooks: web3WalletConnectHooks,
