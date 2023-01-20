0b5ec4dd790f94b9d1faf81cb9f881709e73e8ee
dannydaniil <danny.daniil@uniswap.org>
2023-01-20 11:00:15 -0500

    move connect outside comp

--- web3-react/src/libs/components/Web3ContextProvider.tsx
+++ web3-react/src/libs/components/Web3ContextProvider.tsx
@@ -4,19 +4,19 @@ import React, { ReactNode, useEffect } from 'react'
 
 import { ConnectionType, getConnection, PRIORITIZED_CONNECTORS } from '../connections'
 
-export const Web3ContextProvider = ({ children }: { children: ReactNode }) => {
-  async function connect(connector: Connector) {
-    try {
-      if (connector.connectEagerly) {
-        await connector.connectEagerly()
-      } else {
-        await connector.activate()
-      }
-    } catch (error) {
-      console.debug(`web3-react eager connection error: ${error}`)
+async function connect(connector: Connector) {
+  try {
+    if (connector.connectEagerly) {
+      await connector.connectEagerly()
+    } else {
+      await connector.activate()
     }
+  } catch (error) {
+    console.debug(`web3-react eager connection error: ${error}`)
   }
+}
 
+export const Web3ContextProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     connect(getConnection(ConnectionType.NETWORK).connector)
     connect(getConnection(ConnectionType.GNOSIS_SAFE).connector)
